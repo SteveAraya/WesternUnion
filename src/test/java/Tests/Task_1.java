a2861030e8da2c756e6edf73c0f7eed5d8cdd25e
Steve Araya Solórzano <stevenaraya.24@gmail.com>
2022-10-17 21:08:53 -0600

    Working on the project structure

---
+++ src/test/java/Tests/Task_1.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class Task_1 {
+	
+//	1.	Navigate to https://www.westernunion.com/lt/en/home.html
+//	2.	Expand Burger menu
+//	3.	Select Settings page
+//	4.	Change WU.com Country to United States
+//	5.	Assert that correct country page was loaded.
+
+}
---
+++ src/test/java/Tests/Task_2.java
@@ -0,0 +1,9 @@
+package Tests;
+
+public class Task_2 {
+	
+//	As a UAT tester using https://www.westernunion.com/lt/en/home.html
+//	I want automation tool to search for open agent locations near my Zip code 08247
+//	So that I could get address details of the first location printed in console
+
+}
